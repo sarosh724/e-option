4ef33e649772c79d0c162c672b3cb932a01365b2
sarosh <sarosh>
2023-08-11 16:29:30 +0500

    initial setup

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@eoption.com',
+            'email_verified_at' => now(),
+            'is_admin' => 1,
+            'password' => Hash::make('admin@eoption'), // admin@eoption
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
