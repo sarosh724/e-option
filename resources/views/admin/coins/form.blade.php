8e675eb7e4ae07ee1c0206a2a20ed2eef37e77c2
Sayyam-Ipsum <ahmed.sayyam@ipsum.co.uk>
2023-08-16 15:20:24 +0500

    coin fixes

--- resources/views/admin/coins/form.blade.php
+++ resources/views/admin/coins/form.blade.php
@@ -16,9 +16,39 @@
             <div class="fv-row">
                 <div class="field">
                     <label for="price" class="form-label">
-                        <span class="required">Price</span>
+                        <span class="required">Base Price</span>
                     </label>
                     <input type="number" step="any" class="form-control" id="price" name="price" value="{{@$coin->price}}">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-12">
+            <div class="fv-row">
+                <div class="field">
+                    <label for="min_value" class="form-label">
+                        <span class="required">Minimum Price</span>
+                    </label>
+                    <input type="number" step="any" class="form-control" id="min_value" name="min_value" value="{{@$coin->min_value}}">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-12">
+            <div class="fv-row">
+                <div class="field">
+                    <label for="max_value" class="form-label">
+                        <span class="required">Maximum Price</span>
+                    </label>
+                    <input type="number" step="any" class="form-control" id="max_value" name="max_value" value="{{@$coin->max_value}}">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-12">
+            <div class="fv-row">
+                <div class="field">
+                    <label for="profit_percentage" class="form-label">
+                        <span class="required">Profit Percentage</span>
+                    </label>
+                    <input type="number" step="any" class="form-control" id="profit_percentage" name="profit_percentage" value="{{@$coin->profit_percentage}}">
                 </div>
             </div>
         </div>
@@ -37,7 +67,20 @@
         $("#coin_form").validate({
             rules: {
                 price: {
-                    required: true
+                    required: true,
+                    min: 1
+                },
+                min_value: {
+                    required: true,
+                    min: 1
+                },
+                max_value: {
+                    required: true,
+                    min: 1
+                },
+                profit_percentage: {
+                    required: true,
+                    min: 1
                 },
                 name: {
                     required: true
@@ -45,7 +88,20 @@
             },
             messages: {
                 price: {
-                    required: 'Price is required'
+                    required: 'Base Price is required',
+                    min: "Value must be greater then 0"
+                },
+                min_value: {
+                    required: 'Minimum Price is required',
+                    min: "Value must be greater then 0"
+                },
+                max_value: {
+                    required: 'Maximum Price is required',
+                    min: "Value must be greater then 0"
+                },
+                profit_percentage: {
+                    required: 'Profit Percentage is required',
+                    min: "Value must be greater then 0"
                 },
                 name: {
                     required: "Enter coin name"
--- resources/views/site/pages/trading.blade.php
+++ resources/views/site/pages/trading.blade.php
@@ -6,4 +6,38 @@
 
 @section('content')
     @include('site.sections.breadcrumb', ['title' => 'trading'])
+    <div class="container my-4" id="container"></div>
+
+@stop
+
+@section('scripts')
+    <script src="{{asset('assets/site/highcharts/highstock.js')}}"></script>
+    <script src="{{asset('assets/site/highcharts/highstock.js')}}"></script>
+    <script src="{{asset('assets/site/highcharts/hollowcandlestick.js')}}"></script>
+    <script src="{{asset('assets/site/highcharts/accessibility.js.js')}}"></script>
+    <script>
+        (async () => {
+
+            // Load the dataset
+            const data = await fetch(
+                'https://demo-live-data.highcharts.com/aapl-ohlcv.json'
+            ).then(response => response.json());
+
+            Highcharts.stockChart('container', {
+                rangeSelector: {
+                    selected: 1
+                },
+                navigator: {
+                    series: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                series: [{
+                    type: 'hollowcandlestick',
+                    name: 'Hollow Candlestick',
+                    data: data
+                }]
+            });
+        })();
+    </script>
 @stop
