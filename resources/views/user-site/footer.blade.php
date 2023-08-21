577d3472e718bf689523d7f8e01538d3b664f457
Sayyam-Ipsum <ahmed.sayyam@ipsum.co.uk>
2023-08-21 16:21:56 +0500

    new user site template setup

--- resources/views/user-site/footer.blade.php
+++ resources/views/user-site/footer.blade.php
@@ -1,7 +1,57 @@
-<script src="{{asset('assets/user/js/jquery-1.12.4.min.js')}}"></script>
+<hr style="background: gray; margin: 0;">
+<div class="container py-4">
+    <div class="row py-4">
+        <div class="col-md-3 col-sm-6 col-xs-6 footer-col">
+            <h6>Affiliates</h6>
+            <ul>
+                <li><a href="#">Sign up</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-6 footer-col">
+            <h6>About us</h6>
+            <ul>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-6 footer-col">
+            <h6>FAQ</h6>
+            <ul>
+                <li><a href="#">General questions</a></li>
+                <li><a href="#">Financial questions</a></li>
+                <li><a href="#">Verification</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-6 footer-col">
+            <h6>Regulations</h6>
+            <ul>
+                <li><a href="#">Privacy policy</a></li>
+                <li><a href="#">Payment policy</a></li>
+                <li><a href="#">Service agreement</a></li>
+                <li><a href="#">Risk disclosure</a></li>
+                <li><a href="#">Rules of trading operations</a></li>
+                <li><a href="#">Non-trading operation regulations</a></li>
+            </ul>
+        </div>
+    </div>
+    <hr style="background: gray; margin: 0;">
+    <div class="footer-txt py-4">
+        <p>Maxbit LLC. Address: First Floor, First St Vincent Bank LTD Building, James Street, Kingstown, St. Vincent and Grenadines. </p>
+        <br>
+        <p>The website services are not available in a number of countries, including USA, Canada, Hong Kong, EEA countries, Russia as well as for persons under 18 years of age. </p>
+        <br>
+        <p>
+            Risk Warning: Trading Forex and Leveraged Financial Instruments involves significant risk and can result in the loss of your invested capital. You should not invest more than you can afford to lose and should ensure that you fully understand the risks involved. Trading leveraged products may not be suitable for all investors. Trading non-leveraged products such as stocks also involves risk as the value of a stock can fall as well as rise, which could mean getting back less than you originally put in. Past performance is no guarantee of future results. Before trading, please take into consideration your level of experience, investment objectives and seek independent financial advice if necessary. It is the responsibility of the Client to ascertain whether he/she is permitted to use the services of the Quotex brand based on the legal requirements in his/her country of residence.
+        </p>
+        <br><br>
+        <p>Maxbit LLC is the owner of the qxbroker.com domain.</p>
+        <p>Copyright © 2023 Quotex. All rights reserved</p>
+    </div>
+</div>
 
-<script src="{{asset('assets/user/js/popper.min.js')}}"></script>
-<script src="{{asset('assets/user/js/bootstrap.min.js')}}"></script>
+
+<script src="{{asset('assets/user/js/jquery-1.12.4.min.js')}}"></script>
+{{--<script src="{{asset('assets/user/js/popper.min.js')}}"></script>--}}
+{{--<script src="{{asset('assets/user/js/bootstrap.min.js')}}"></script>--}}
 <script src="{{asset('assets/user/js/bootstrap.bundle.min.js')}}"></script>
 <script src="{{asset('assets/user/js/jquery.validate.min.js')}}"></script>
 
